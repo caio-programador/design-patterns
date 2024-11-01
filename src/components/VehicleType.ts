809dc6c5340d3f0da8ec13e223258470b1db6e94
caio-programador <caio.florees2005@gmail.com>
2024-11-01 13:26:52 -0300

    Criacional - builder

---
+++ src/components/VehicleType.ts
@@ -0,0 +1,10 @@
+enum VehicleType {
+  SEDAN = 'Sedan',
+  SPORTCAR = 'Sportcar',
+  SUV = 'SUV',
+  PICKUPTRUCK = 'PickupTruck',
+  TRUCK = 'Truck',
+  MOTORCYCLE = 'Motorcycle'
+}
+
+export default VehicleType;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-const curso: string = "Design Patterns com TypeScript";
+import { VehicleBuilder } from "./builder/VehicleBuilder";
+import Director from "./director/Director";
+import Vehicle from "./products/Vehicle";
 
-console.log(curso);
+const builder: VehicleBuilder = new VehicleBuilder();
+const director: Director = new Director(builder);
+
+director.constructSedan();
+
+const sedan: Vehicle = builder.getVehicle();
+
+console.log(sedan);
+
+director.constructTruck();
+const truck = builder.getVehicle();
+
+console.log(truck);
+
+director.constructMotorcycle();
+const motorcycle = builder.getVehicle();
+console.log(motorcycle);
